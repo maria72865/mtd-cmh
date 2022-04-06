067b5c580a41974d79277e811efa758e9b83c25c
Maria Dahlquist <maria72865@gmail.com>
2022-04-06 16:17:16 -0500

    Updates

---
+++ precache-manifest.e4c1c7ac3c5f35c98dc223e656d70210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa891198665a4d82c062b44fb45cbfc",
+    "url": "/mtd-cmh/index.html"
+  },
+  {
+    "revision": "b50ac0b1de5537411a37",
+    "url": "/mtd-cmh/static/js/2.c4cfa19f.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/mtd-cmh/static/js/2.c4cfa19f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ddd71f39dc44665749",
+    "url": "/mtd-cmh/static/js/main.b603e9f4.chunk.js"
+  },
+  {
+    "revision": "afc400424acc0dde12af",
+    "url": "/mtd-cmh/static/js/runtime-main.ea439601.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/mtd-cmh/static/media/commerce.422fec10.png"
+  }
+]);
